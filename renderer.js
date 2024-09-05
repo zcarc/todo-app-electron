56a48bd212cf6db516eff60270d176d43d2eafca
Hyunsoo Lee <zcarc@naver.com>
2024-09-06 03:44:33 +0900

    추가: todo 수정 버튼, 수정 input, prettier

--- renderer.js
+++ renderer.js
@@ -1,46 +1,53 @@
-const { ipcRenderer } = require("electron");
+const { ipcRenderer } = require('electron')
 
 function todoAppData() {
-  return {
-    todoInput: "",
-    todos: [],
+    return {
+        todoInput: '',
+        todos: [],
+        editingId: 0,
 
-    async init() {
-      this.todos = await ipcRenderer.invoke("get-todos");
-    },
+        editingInput: '',
 
-    toggleTodoCompleted(todoId) {
-      this.todos = this.todos.map((todo) => {
-        if (todo.id === todoId) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      });
-      this.saveTodos();
-    },
+        editTodo(id, task) {
+            this.editingId = id
+            this.editingInput = task
+        },
+        async init() {
+            this.todos = await ipcRenderer.invoke('get-todos')
+        },
 
-    addTodo() {
-      const task = this.todoInput.trim();
-      if (task) {
-        const date = new Date();
-        this.todos.push({
-          id: this.todos.length + 1,
-          year: date.getFullYear(),
-          month: date.getMonth() + 1,
-          date: date.getDate(),
-          hour: date.getHours(),
-          minutes: date.getMinutes(),
-          task,
-          completed: false,
-        });
-        this.saveTodos();
-        this.todoInput = "";
-      }
-    },
+        toggleTodoCompleted(todoId) {
+            this.todos = this.todos.map((todo) => {
+                if (todo.id === todoId) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            this.saveTodos()
+        },
 
-    saveTodos() {
-      const list = this.todos.map((e) => Object.assign({}, e));
-      ipcRenderer.invoke("save-todos", list);
-    },
-  };
+        addTodo() {
+            const task = this.todoInput.trim()
+            if (task) {
+                const date = new Date()
+                this.todos.push({
+                    id: this.todos.length + 1,
+                    year: date.getFullYear(),
+                    month: date.getMonth() + 1,
+                    date: date.getDate(),
+                    hour: date.getHours(),
+                    minutes: date.getMinutes(),
+                    task,
+                    completed: false,
+                })
+                this.saveTodos()
+                this.todoInput = ''
+            }
+        },
+
+        saveTodos() {
+            const list = this.todos.map((e) => Object.assign({}, e))
+            ipcRenderer.invoke('save-todos', list)
+        },
+    }
 }
